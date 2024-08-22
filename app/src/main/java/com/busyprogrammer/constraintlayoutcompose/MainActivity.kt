9db23faaa9c9786649ff5d2536c8c4c1f5071cbc
elianismedina <elianismedina05@outlook.com>
2024-08-22 07:25:03 -0500

    created welcome screen

--- app/src/main/java/com/busyprogrammer/constraintlayoutcompose/MainActivity.kt
+++ app/src/main/java/com/busyprogrammer/constraintlayoutcompose/MainActivity.kt
@@ -6,7 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
+
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -29,10 +29,6 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
 @Preview(showBackground = true)
 @Composable
